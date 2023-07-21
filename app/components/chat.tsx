eebc418fbf0954a5e14f4ec01d0350f372203559
walkthunder Cooper <walkthunder@163.com>
2023-07-21 13:41:50 +0800

    fix(ads): Remove feeds.

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -68,7 +68,6 @@ import { useCommand } from "../command";
 import { prettyObject } from "../utils/format";
 import { ExportMessageModal } from "./exporter";
 import { getClientConfig } from "../config/client";
-import AdFeed from "./AdFeed";
 
 const Markdown = dynamic(async () => (await import("./markdown")).Markdown, {
   loading: () => <LoadingIcon />,
@@ -817,11 +816,8 @@ export function Chat() {
 
           const shouldShowClearContextDivider = i === clearContextIndex - 1;
 
-          const addAds = i % 3 === 0 && !isUser;
-
           return (
             <>
-              {addAds ? <AdFeed idx={i} /> : null}
               <div
                 key={i}
                 className={
