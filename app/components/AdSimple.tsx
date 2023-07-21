7b04ab1592a168fab1f716bbcf3e95a64f0a900c
walkthunder Cooper <walkthunder@163.com>
2023-07-21 10:18:34 +0800

    feat(ads): Add close ads button.

--- app/components/AdSimple.tsx
+++ app/components/AdSimple.tsx
@@ -1,14 +1,37 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function AdSimple(...props: any) {
   const { currentPath } = props;
+  const [hideAds, setHideAds] = useState(false);
+
   useEffect(() => {
     (window as any).adsbygoogle = (window as any).adsbygoogle || [];
     (window as any).adsbygoogle.push({});
   }, [currentPath]);
 
-  return (
-    <div key={currentPath} style={{ width: "calc((100vw - 1224px))" }}>
+  const closeAds = () => {
+    setHideAds(true);
+  };
+
+  return hideAds ? null : (
+    <div
+      key={currentPath}
+      style={{ position: "relative", width: "calc((100vw - 1224px))" }}
+    >
+      <div
+        onClick={closeAds}
+        style={{
+          position: "absolute",
+          top: "-20px",
+          right: "20px",
+          cursor: "pointer",
+          boxShadow: "0 0 2px 1px #80808066",
+          padding: "4px 6px",
+          borderRadius: "4px",
+        }}
+      >
+        关闭广告
+      </div>
       <ins
         className="adsbygoogle"
         style={{ display: "block" }}
