78f13d946a685446c3f01d01e9b0c55c09fd29a8
walkthunder Cooper <walkthunder@163.com>
2023-07-24 09:46:43 +0800

    fix(ads): Remove `signalGooglefcPresent`

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,16 +54,16 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3614870144525266"
           crossOrigin="anonymous"
         ></Script>
-        <Script
+        {/* <Script
           async
           src="https://fundingchoicesmessages.google.com/i/pub-3614870144525266?ers=1"
           nonce="noZUSv6xBJD0HD6gHh2jSw"
-        ></Script>
-        <Script nonce="noZUSv6xBJD0HD6gHh2jSw" id="ads-blocker-alert">
+        ></Script> */}
+        {/* <Script nonce="noZUSv6xBJD0HD6gHh2jSw" id="ads-blocker-alert">
           {`
           (function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
           `}
-        </Script>
+        </Script> */}
       </head>
       <body>{children}</body>
     </html>
